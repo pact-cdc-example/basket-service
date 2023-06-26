b94221b21f4f0a7a0d01bd519c19b050917d7c0d
eneskzlcn <enes@arf.one>
2023-06-26 21:01:52 +0300

    funding repostiory get/list impl. and tests done.

--- can-i-deploy.sh
+++ can-i-deploy.sh
@@ -5,8 +5,11 @@ set -x
 VERSION=0.0.1
 PACTICIPANT=BasketService
 BROKER_BASE_URL=http://localhost
+ENV=dev
+#
+#pact-broker can-i-deploy \
+#--pacticipant=${PACTICIPANT} \
+#--broker-base-url=${BROKER_BASE_URL} \
+#--latest
 
-pact-broker can-i-deploy \
---pacticipant=${PACTICIPANT} \
---version=${VERSION} \
---broker-base-url=${BROKER_BASE_URL}
+pact-broker can-i-deploy --pacticipant ${PACTICIPANT} --version ${VERSION} --to dev --retry-while-unknown=12 --retry-interval=10 --limit 1000
